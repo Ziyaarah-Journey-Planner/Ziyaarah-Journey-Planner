4b5bb30d82523c16b8a42c144f01ec5c9f4946c1
amiin5377-amo <amiin5377@gmail.com>
2026-04-07 20:42:48 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import Header from './Layout/Header';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
-import Footer from './Layout/Footer';
-import HomePage from './Pages/HomePage';
-import LoginPage from './Pages/LoginPage';
-import RegisterPage from './Pages/RegisterPage';            
-import './App.css'
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
 
 function App() {
  
 
   return (
-    <div>
-      <Header />
-      <Router>
-        
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
+    <> 
+      <Navbar />
+      <Hero />
+    </>
   )
 }
 
